bb37adc786305b284a1f3a7c0a84bb1b98633513
Nightchicken <22nightchicken@gmail.com>
2024-11-17 18:34:13 -0600

    Finished Seller File functions

--- Assignment2Skeleton/Assignment2/Category/src/Seller.java
+++ Assignment2Skeleton/Assignment2/Category/src/Seller.java
@@ -13,18 +13,25 @@ public class Seller {
 	// params: (String)
 	//
 	Seller (String name){
+		this.name = name;
+		this.salesTotal = 0.0f;
+		this.lapTopSold = 0;
+		this.deskTopSold = 0;
+		this.tabletSold = 0;
 	}
 	// Returns true if the seller's name is the same as nameToSearch.
 	// false otherwise.
 	// params: (String)
 	//
 	public boolean SellerHasName (String nameToSearch){
+		return (this.name.equals(nameToSearch));
 	}
 	// Returns true if the seller sold at least 12 computers
 	// or has a sale amount of at least $35,000.00.
 	// params: (none)
 	//
 	public boolean WinsPrize (){
+		return(this.salesTotal >= 35000 || this.lapTopSold + this.deskTopSold + this.tabletSold >= 12);
 	}
 	//
 	// Print the salesperson's name, sales total, and number of
@@ -32,6 +39,7 @@ public class Seller {
 	// params: (none)
 	//
 	public String toString( ){
+		return String.format("%s sold %d of computers for a total of $%f",this.name, this.deskTopSold + this.lapTopSold + this.tabletSold, this.salesTotal);
 	}
 	// Adds the money and number of computers to the seller's accumulated
 	// sales total and number of computers sold based on the computer type.
@@ -41,5 +49,19 @@ public class Seller {
 	// “TABLET” then the tablet fields are updated by numComputers.
 	// Param: ( double , int , String )
 	public void UpdateSales ( double totalDollars, int numComputers,String computerType ){
+		this.salesTotal += totalDollars;
+		switch(computerType){
+			case("TABLET"):
+				this.tabletSold += numComputers;
+				break;
+			case("DESKTOP"):
+				this.deskTopSold += numComputers;
+				break;
+			case("LAPTOP"):
+				this.lapTopSold += lapTopSold;
+				break;
+			}
+
+		}
 	}
 }// end class Seller
