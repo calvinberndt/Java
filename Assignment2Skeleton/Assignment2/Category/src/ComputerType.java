e7d3b11e913a602ca24dbdafc471289b9365399f
Nightchicken <22nightchicken@gmail.com>
2024-12-08 18:02:33 -0600

    I believe I finished the Cash commands

--- Assignment2Skeleton/Assignment2/Category/src/ComputerType.java
+++
@@ -1,5 +0,0 @@
-
-
-public enum ComputerType {
-	DESKTOP, LAPTOP, TABLET;
-}
--- Assignment2Skeleton/Assignment2/Category/src/MainClass.java
+++
@@ -1,7 +0,0 @@
-public class MainClass {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		SellerList GBStore = new SellerList();
-		SellerList.processCommands();
-	}
-} // end of MainClass
